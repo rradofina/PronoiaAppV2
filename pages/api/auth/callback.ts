20455c04f74f3d89b7de8a107380bee8157b5b0c
rradofina <rradofina@gmail.com>
2025-07-17 02:01:54 +0800

    Update callback.ts

--- pages/api/auth/callback.ts
+++ pages/api/auth/callback.ts
@@ -8,12 +8,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
   const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
-  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
-
-  // --- Final Vercel Debugging ---
-  res.status(500).send(`DEBUG INFO: Client Secret on Vercel is: '${clientSecret}'`);
-  return;
-  // --- End Final Vercel Debugging ---
+  const clientSecret = process.env.GOOGLE_OAUTH_SECRET;
 
   // Determine the redirect URI based on the environment
   const redirectUri = process.env.NODE_ENV === 'production'
